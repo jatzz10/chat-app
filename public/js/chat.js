8ebd3ff7cc52f90ecd51b50b1c64e4ebb88fbcf1
Jatin Sharma <jatinsharma2751997@gmail.com>
2019-01-14 22:05:11 +0530

    Updated chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,20 @@
 $(function () {
   let socket = io();
     
-  // When user submit the message
+  // When the user submit the message
   $('form').submit(function() { 
     let data = {
       name: $('#userName').val(), 
       message: $('#clientMessage').val()
     };
 
-    socket.emit('chat-message', data);  // Client sending messages to the Server
+    socket.emit('chat-message', data);  // Client sending the message to the Server
     $('#userName').prop('disabled', true);
     $('#clientMessage').val('');
     return false;
   });
 
-  // Server updating the messages of the Client's side
+  // Client socket listening to the event emitted by the server
   socket.on('chat-message', function(data) {
     $('#messages').append($('<li>').text(data.name + ": " + data.message));
   });
